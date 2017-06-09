e084043dbadd48ad9e0c6dfc4ea02c74b9470055
Bagaria <ABagaria@elementcorp.com>
2017-06-09 11:15:39 +0530

    testing

--- src/test2.js
+++ src/test2.js
@@ -1,6 +1,6 @@
 function greeting(fName) {
 	/* jshint ignore:start */
-	var age = 20;
+	//var age = 20;
 	/* jshint ignore:end */
 	if (fName && fName.length > 0 && age === 20) {
 		return "Hello " + fName + "! Welcome to pre-commit world.";
