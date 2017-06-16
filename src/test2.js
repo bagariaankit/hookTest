b9145a829f0c970980c9e98aa7f8f949511f46ae
Bagaria <ABagaria@elementcorp.com>
2017-06-16 14:25:17 +0530

    Testing commit.

--- src/test2.js
+++ src/test2.js
@@ -1,8 +1,10 @@
 function greeting(fName) {
 	var age;
-	if (fName && fName.length > 0 && age == 20) {
-		return "Hello " + fName + "! Welcome to pre-commit world."
+	if (fName && fName.length > 0 && age === 20) {
+		return "Hello " + fName + "! Welcome to pre-commit world.";
 	} else {
 		return false;
 	}
 }
+
+greeting();
